26f5e9c75755d75ce434941ee7a88a9b6f800ce2
EXPERT <saievmuhammad@mail.ru>
2021-06-21 19:35:00 +0300

    Initial commit

---
+++ src/main/java/ClientHandler.java
@@ -0,0 +1,62 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private final Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+
+    @Override
+    public void run() {
+        try (
+                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+                DataInputStream dis = new DataInputStream(socket.getInputStream())
+        ) {
+            System.out.printf("Client %s connected\n", socket.getInetAddress());
+            while (true) {
+                String command = dis.readUTF();
+                if ("upload".equals(command)) {
+                    try {
+                        File file = new File("server"  + File.separator + dis.readUTF());
+                        if (!file.exists()) {
+                            file.createNewFile();
+                        }
+                        FileOutputStream fos = new FileOutputStream(file);
+
+                        long size = dis.readLong();
+
+                        byte[] buffer = new byte[8 * 1024];
+
+                        for (int i = 0; i < (size + (buffer.length - 1)) / (buffer.length); i++) {
+                            int read = dis.read(buffer);
+                            fos.write(buffer, 0, read);
+                        }
+                        fos.close();
+                        dos.writeUTF("OK");
+                    } catch (Exception e) {
+                        dos.writeUTF("FATAL ERROR");
+                    }
+                }
+
+                if ("download".equals(command)) {
+                    // TODO: 14.06.2021
+                }
+                if ("exit".equals(command)) {
+                    System.out.printf("Client %s disconnected correctly\n", socket.getInetAddress());
+                    break;
+                }
+
+                System.out.println(command);
+                dos.writeUTF(command);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
